3234e51c8ee45663b5a7a604bcdc92e3f5a676f5
Artur <artur.mkr@gmail.com>
2023-08-11 16:52:29 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='calculator',
+    version='0.0.1',
+    author='artur',
+    author_email='',
+    url='',
+    description='calculator',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
